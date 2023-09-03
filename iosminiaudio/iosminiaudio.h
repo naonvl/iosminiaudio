77c9d5b5f1cc6d43845b550a6cb4b9dc259eb0d7
nao <chiwoo@postech.ac.kr>
2023-09-03 16:37:25 +0700

    first commit

---
+++ iosminiaudio/iosminiaudio.h
@@ -0,0 +1,13 @@
+//
+//  iosminiaudio.h
+//  iosminiaudio
+//
+//  Created by Nata on 03/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iosminiaudio : NSObject
++ (void)logMessage;
+
+@end
---
+++ iosminiaudio/iosminiaudio.m
@@ -0,0 +1,14 @@
+//
+//  iosminiaudio.m
+//  iosminiaudio
+//
+//  Created by Nata on 03/09/23.
+//
+
+#import "iosminiaudio.h"
+
+@implementation iosminiaudio
++ (void)logMessage {
+    NSLog(@"IT IS WORKING");
+}
+@end
